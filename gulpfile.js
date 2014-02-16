532787ab54fe1a36d74c29efab2106d7f07543d0
dickeyxxx <jeff@dickeyxxx.com>
2014-02-15 16:59:56 -0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var liveScript = require('gulp-livescript');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+
+gulp.task('default', ['scripts']);
+
+gulp.task('scripts', function() {
+  gulp.src('./scripts/*.ls')
+    .pipe(liveScript({bare: true}).on('error', gutil.log))
+    .pipe(uglify())
+    .pipe(concat('site.min.js'))
+    .pipe(gulp.dest('./dist/js'));
+});
